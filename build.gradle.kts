254fbf6212aa1c9297ec6161b2512bae75a9ffd3
booky10 <boooky10@gmail.com>
2023-07-22 02:26:03 +0200

    Add buildscript with paperweight patcher

---
+++ build.gradle.kts
@@ -0,0 +1,69 @@
+plugins {
+    id("java-library")
+    id("com.github.johnrengelman.shadow") version "8.1.1" apply false
+    id("io.papermc.paperweight.patcher") version "1.5.5"
+}
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/") {
+        content { onlyForConfigurations("paperclip") }
+    }
+}
+
+dependencies {
+    remapper("net.fabricmc:tiny-remapper:0.8.6:fat")
+    decompiler("net.minecraftforge:forgeflower:2.0.627.2")
+    paperclip("io.papermc:paperclip:3.0.3")
+}
+
+subprojects {
+    apply<JavaLibraryPlugin>()
+    apply<MavenPublishPlugin>()
+
+    java {
+        withSourcesJar()
+
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(17))
+            vendor.set(JvmVendorSpec.ADOPTIUM)
+        }
+    }
+
+    tasks.withType<JavaCompile> {
+        options.encoding = Charsets.UTF_8.name()
+        options.release.set(17)
+    }
+
+    repositories {
+        mavenCentral()
+
+        maven("https://oss.sonatype.org/content/groups/public/")
+        maven("https://repo.papermc.io/repository/maven-public/")
+        maven("https://ci.emc.gs/nexus/content/groups/aikar/")
+        maven("https://repo.aikar.co/content/groups/aikar")
+        maven("https://repo.md-5.net/content/repositories/releases/")
+        maven("https://hub.spigotmc.org/nexus/content/groups/public/")
+        maven("https://jitpack.io/")
+    }
+}
+
+paperweight {
+    serverProject.set(project(":CloudPlane-Server"))
+
+    remapRepo.set("https://maven.fabricmc.net/")
+    decompileRepo.set("https://files.minecraftforge.net/maven/")
+
+    usePaperUpstream(providers.gradleProperty("paperRef")) {
+        withPaperPatcher {
+            apiPatchDir.set(layout.projectDirectory.dir("patches${File.separator}api"))
+            serverPatchDir.set(layout.projectDirectory.dir("patches${File.separator}server"))
+
+            apiOutputDir.set(layout.projectDirectory.dir("CloudPlane-API"))
+            serverOutputDir.set(layout.projectDirectory.dir("CloudPlane-Server"))
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://repo.papermc.io/repository/maven-public/")
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
+}
+
+rootProject.name = "CloudPlane"
+include("CloudPlane-API", "CloudPlane-Server", "test-plugin")
