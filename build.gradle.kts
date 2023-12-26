7ee1358f8d395cd07ece49d8728e9a94825b4af2
booky10 <boooky10@gmail.com>
2023-12-26 04:35:16 +0100

    Updated Upstream (Paper)
    Upstream has released updates that appear to apply and compile correctly
    
    Paper Changes:
    PaperMC/Paper@4d111a3 Un-experimentalize Entity TP APIs (#9964)
    PaperMC/Paper@2182d47 Update mapping-io (#9975)
    PaperMC/Paper@9993eb9 Updates documentation in EntityEquipment.java (#9992)
    PaperMC/Paper@2184fbc Log correct recipes and advancement count on server start (#9978)
    PaperMC/Paper@8bda1f7 Remove unnecessary durability check in ItemStack#isSimilar (#9979)
    PaperMC/Paper@977a729 [ci skip] Correct Windows requiring path to gradlew (#9976)
    PaperMC/Paper@3766afa [ci skip] Add mention of FQ imports (#9994)
    PaperMC/Paper@40872ec Fix CraftMetaItem#addAttributeModifier duplication check (#9995)
    PaperMC/Paper@ffa4115 Configurable Dry and Wet Farmland Tick Rates (#9968)
    PaperMC/Paper@37bee09 Restore vanilla entity drops behavior (#7650)
    PaperMC/Paper@5cbd535 Fix Mushroom cow stew api (#9934)
    PaperMC/Paper@8e061ce Add Structure check API (#9062)
    PaperMC/Paper@9271ee7 Dont resend blocks on interactions (#9413)
    PaperMC/Paper@931781c [ci skip] rebuild patches

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("java-library")
     id("com.github.johnrengelman.shadow") version "8.1.1" apply false
-    id("io.papermc.paperweight.patcher") version "1.5.10"
+    id("io.papermc.paperweight.patcher") version "1.5.11"
 }
 
 repositories {
@@ -12,7 +12,7 @@ repositories {
 }
 
 dependencies {
-    remapper("net.fabricmc:tiny-remapper:0.8.6:fat")
+    remapper("net.fabricmc:tiny-remapper:0.8.10:fat")
     decompiler("net.minecraftforge:forgeflower:2.0.627.2")
     paperclip("io.papermc:paperclip:3.0.3")
 }
