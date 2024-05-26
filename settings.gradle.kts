6e6456288a391c7e212134135cd6a232885a7de9
booky10 <boooky10@gmail.com>
2024-05-27 00:42:06 +0200

    Updated Upstream (Paper)
    Upstream has released updates that appear to apply and compile correctly
    
    Paper Changes:
    PaperMC/Paper@d8d59e0 Fix NPE sending resource pack without prompt
    PaperMC/Paper@5a9afbe Fixes issues in the suppress dismount cancellation patch (#10735)
    PaperMC/Paper@0ad09de Make PaperSimplePluginClassLoader show class which is not found
    PaperMC/Paper@b3b3406 fix CompassMeta not being correct (#10737)
    PaperMC/Paper@591521e Check for more correct profile validation (#10730)
    PaperMC/Paper@7d2e5c3 Add an 'empty' RecipeChoice for certain ingredient slots (#10710)
    PaperMC/Paper@9bf4855 Add a better warning message than "Server performance will be affected" for CommandRegisteredEvent use (#10754)
    PaperMC/Paper@66cb880 Remove ThreadedWorldUpgrader patch
    PaperMC/Paper@980cff9 Fix compile
    PaperMC/Paper@d3ffa62 fix default item attributes
    PaperMC/Paper@b149584 Always show command exception stack traces in logs (#10766)
    PaperMC/Paper@a9201d4 Move logic in our patches to ItemType/BlockType (#10772)
    PaperMC/Paper@3de408e Fix equipment slot and group API (#10767)
    PaperMC/Paper@d408381 Print CommandRegisteredEvent deprecation warnings again (#10756)
    PaperMC/Paper@6de7a1f Improve default item attributes API (#10765)
    PaperMC/Paper@2a90732 Remove incorrect logic for Fireball#setVelocity (#10764)
    PaperMC/Paper@5e7b65a Allow using PluginLoader classpath API from Bukkit plugins (#10758)
    PaperMC/Paper@3004717 Do not re-wrap vanilla goals (#10751)
    PaperMC/Paper@377733d Use getter/setter on Mob for equipment drop chances (#10780)
    PaperMC/Paper@dff591d Allow to define new map cursor types (#10782)
    PaperMC/Paper@4fd3ac0 Update Player#getListeningPluginChannels Jdoc (#10778)
    PaperMC/Paper@535dca5 Fix a few issues with ItemMeta (#10740)
    PaperMC/Paper@26e90b9 Fix CraftMetaBlockState for data components (#10731)
    PaperMC/Paper@f0bd0cc Expose anvil cost in the API (#10682)
    PaperMC/Paper@41bee55 Fix NPE for color-related metas
    PaperMC/Paper@4e10fad reset meta block state on BlockStateMeta#setBlockState
    PaperMC/Paper@a7ae966 readd itemstack data sanitization (#10454)
    PaperMC/Paper@ca50b87 Clear firework item properly (#10793)
    PaperMC/Paper@9774a52 fix and cleanup loot table patches (#10100)
    PaperMC/Paper@ac4ee06 Fix integer overflow for lag compensating eating check (#10797)
    PaperMC/Paper@6649a17 Fix BlockStateMeta equals (#10795)
    PaperMC/Paper@1b8cdc6 Extend Player Interact cancellation to cover Jigsaw blocks (#10719)
    PaperMC/Paper@841ab41 Fix more item desync when an event is cancelled (#10654)
    PaperMC/Paper@b0e3ca4 Prevent NPE if hooked entity was cleared (#10712)
    PaperMC/Paper@347bbe3 Fix Cancelling BlockPlaceEvent calling onRemove (#10773)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,7 @@
+rootProject.name = "CloudPlane"
+
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
-rootProject.name = "CloudPlane"
 include("CloudPlane-API", "CloudPlane-Server", "test-plugin")
